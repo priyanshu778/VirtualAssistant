794b74623de31d6a7a98bad9e3698ccec33ae95a
priyanshu778 <priyanshuk7788@gmail.com>
2025-03-27 16:10:51 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,105 @@
+let btn = document.querySelector("#btn");
+let content = document.querySelector("#content");
+let voice = document.querySelector("#voice");
+
+function speak(text) {
+    let text_speak = new SpeechSynthesisUtterance(text);
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+    text_speak.volume = 1;
+    text_speak.lang = "en-GB";
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    let day = new Date();
+    let hours = day.getHours();
+
+    if (hours >= 0 && hours < 12) {
+        speak("Good Morning Sir!");
+    } else if (hours >= 12 && hours < 16) {
+        speak("Good Afternoon Sir!");
+    } else {
+        speak("Good Evening Sir!");
+    }
+}
+
+window.addEventListener("load", () => {
+    wishMe();
+});
+
+let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let recognition = new speechRecognition();
+
+recognition.onresult = (event) => {
+    let currentIndex = event.resultIndex;
+    let transcript = event.results[currentIndex][0].transcript;
+    content.innerText = transcript;
+    takeCommand(transcript.toLowerCase());
+};
+
+btn.addEventListener("click", () => {
+    recognition.start();
+    btn.style.display = "none";
+    voice.style.display = "block";
+});
+
+function takeCommand(message) {
+    btn.style.display = "flex";
+    voice.style.display = "none";
+
+    if (message.includes("hello") || message.includes("hey")) {
+        speak("Hello Sir, what can I help you with?");
+    }
+    else if (message.includes("who are you")) {
+        speak("I am Shifra, your personal virtual assistant created by Prayash Thakur.");
+    }
+    else if (message.includes("open youtube")) {
+        speak("Opening YouTube...");
+        window.open("https://www.youtube.com/", "_blank");
+    }
+    else if (message.includes("open google")) {
+        speak("Opening Google...");
+        window.open("https://www.google.com/", "_blank");
+    }
+    else if (message.includes("open facebook")) {
+        speak("Opening Facebook...");
+        window.open("https://www.facebook.com/", "_blank");
+    }
+    else if (message.includes("open instagram")) {
+        speak("Opening Instagram...");
+        window.open("https://www.instagram.com/", "_blank");
+    }
+    else if (message.includes("open calculator")) {
+        speak("Opening Calculator...");
+        window.open("calculator://");
+    }
+    else if (message.includes("time")) {
+        let time = new Date().toLocaleString(undefined, { hour: "numeric", minute: "numeric" });
+        speak(time);
+    }
+    else if (message.includes("date")) {
+        let date = new Date().toLocaleString(undefined, { day: "numeric", month: "short" });
+        speak(date);
+    }
+    else if (message.includes("joke")) {
+        tellJoke();
+    }
+    else {
+        let finalText = "I found this on the internet regarding " + message.replace("shifra", "") || message.replace("shipra", "");
+        speak(finalText);
+        window.open(`https://www.google.com/search?q=${message.replace("shifra", "")}`, "_blank");
+    }
+}
+
+
+
+function tellJoke() {
+    fetch('https://official-joke-api.appspot.com/random_joke')
+        .then(response => response.json())
+        .then(data => {
+            let joke = `${data.setup} - ${data.punchline}`;
+            speak(joke);
+        })
+        .catch(error => speak("Sorry, I couldn't fetch a joke at the moment."));
+}
